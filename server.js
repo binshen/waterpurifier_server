3ae90495bc409046a2c362313b07b33d974851dc
沈斌 <bluelazysb@hotmail.com>
2017-03-14 23:03:20 +0800

    代码更新

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ var moment = require('moment');
 var mongoClient = require('mongodb').MongoClient;
 var config = require('./config');
 
+var map = {
+    "5a0000": 100,
+    '6b0000': 50
+};
+
+global.sockets = {};
+global.app_sockets = {};
+
 function handleData(db, socket, value) {
+/*
     if(value.startsWith('5a01')) {
         socket.write(new Buffer([0x6a, 0x01, 0xff, 0xff, 0xff, 0xff, 0x01, 0xa2]));
 
@@ -18,14 +27,40 @@ function handleData(db, socket, value) {
             }
         }, 6000);
     }
+*/
+    var mac = value.slice(6,18).toLowerCase();
+    if(value.startsWith('5a0000')) {
+
+        return;
+    }
+
+    if(value.startsWith('6b0000')) {
+
+        return;
+    }
 }
 
 function doWork(db, socket, data) {
     if(data == "") return;
+    var length = map[data.slice(0, 6)];
+    if(length > 0) {
+        var value = data.slice(0, length);
+        if(value.length >= 18) {
+            var mac = value.slice(6,18).toLowerCase();
+            if(value.slice(0,6) == "6b0000") {
+                if(app_sockets[mac] == undefined || app_sockets[mac] == null) {
+                    app_sockets[mac] = [];
+                }
+                app_sockets[mac].push(socket);
+            } else {
+                sockets[mac] = socket;
+            }
+        }
 
-    handleData(db, socket, data.slice(0, 12));
+        handleData(db, socket, value);
 
-    doWork(db, socket, data.slice(12));
+        doWork(db, socket, data.slice(length));
+    }
 }
 
 mongoClient.connect(config.URL, function(err, db) {
